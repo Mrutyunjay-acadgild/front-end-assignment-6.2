f6e8c3ebe996f6102209d3105872fe4235d24377
Mrutyunjay-acadgild <nayak.mrutyunjay3@gmail.com>
2016-12-20 23:48:30 +0530

    Add files via upload

---
+++ app5.js
@@ -0,0 +1,21 @@
+var num1 = 6;
+var num2 = 3;
+var num3 =9;
+console.log("First number is ="+num1);
+console.log("Second number is ="+num2);
+console.log("Third number is ="+num3);
+if(num1>num2 && num1>num3){
+    console.log(num1 + " is the largest of the three numbers");
+    
+}
+else if(num2>num1 && num2>num3){
+  console.log(num2 + " is the largest of the three numbers");  
+    
+}
+else{
+    
+  console.log(num3 + " is the largest of the three numbers");    
+}
+
+
+
